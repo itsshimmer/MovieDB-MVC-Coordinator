a1981912400bf9fe5006b7bff9844f2dd82122a8
João Brentano <joaobrentano.br@gmail.com>
2022-03-03 15:44:28 -0300

    Initial Commit

---
+++ MovieDB-MVC-Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB-MVC-Coordinator
+//
+//  Created by João Brentano on 03/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
